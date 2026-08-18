c707eaac0a4abc3d16f8d5334259b47abf8467af
fateh-tryingnewthings <mdfatehatul.hafiz.06@gmail.com>
2026-08-19 01:35:25 +0800

    Create firstfile.php
    Corrected a typo in the heading generated by PHP.

---
+++ firstfile.php
@@ -0,0 +1,16 @@
+<?php
+// Basic echo script
+echo "Hello, Ninjas!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First PHP File</title>
+</head>
+<body>
+    <h1><?php echo "This headind is by PHP!"; ?></h1>
+    <p><?php echo "PHP seamlessly integrates into HTML markup."; ?></p>
+</body>
+</html>

cc39d1d382a3bfc4d7fb3c27366c30f15f635316
fateh-tryingnewthings <mdfatehatul.hafiz.06@gmail.com>
2026-08-19 01:28:08 +0800

    Add introductory PHP tutorial notes

---
+++ 01_notes.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  ============================
+  TUTORIAL #1: WHY LEARN PHP? & HOW IT WORKS
+  ============================
+  - PHP stands for Hypertext Preprocessor
+  - PHP is a server-side scripting language.
+  - PHP executes entirely on the server unlike HTML that run on the client's browser
+  - The server processes the PHP code and returns HTML to the browser.
+  - Used for: Dynamic content, database interactions (MySQL), session tracking, 
+    form handling, and building full-stack web application0s
+*/
+
+echo "<h1>File Number 1: Introduction to PHP</h1>";
+echo "<p>PHP is running on the server and rendering dynamic content to the browser!</p>";
+?>
